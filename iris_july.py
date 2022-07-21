bd28e6d7f9bc4636abdee6b9e23bcc67ede8211e
SSofiah <109711189+SSofiah@users.noreply.github.com>
2022-07-21 15:35:43 +0800

    Update iris_july.py

--- iris_july.py
+++ iris_july.py
@@ -40,11 +40,11 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
+st.write(['Iris-setosa','Iris-versicolor','Iris-virginica'])
 
 st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-#st.write(prediction)
+#st.write(iris.target_names[prediction])
+st.write(prediction)
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
